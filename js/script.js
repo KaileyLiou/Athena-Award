25050b716d5bf150bc3fb85e9e971959fe5957c2
KaileyLiou <kaileyliou09@gmail.com>
2025-07-16 11:56:02 -0400

    added string

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var Typed = new Typed(".typed-text", {
-    strings: ["a high schooler.", "a coder", "an athlete.", "a graphic designer."],
+    strings: ["a high schooler.", "a coder.", "an athlete.", "a graphic designer."],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
