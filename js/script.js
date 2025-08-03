1d7d26b709a26c890734725c37e1bdf84202ad6b
KaileyLiou <kaileyliou09@gmail.com>
2025-08-02 22:37:09 -0400

    added themes

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ function toggleCaption() {
     caption.style.display = "none";
   }
 }
+
+const toggleButton = document.getElementById("theme-toggle");
+const themeIcon = document.getElementById("theme-icon");
+
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeIcon.src = "assets/moon.svg";
+    } else {
+        themeIcon.src = "assets/sun.svg";
+    }
+});
